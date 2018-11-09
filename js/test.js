043d26c02fccce8d316e8da77a507d2cc352e91d
fu1252 <fu1252@163.com>
2018-11-09 16:50:33 +0800

    test1

---
+++ js/test.js
@@ -0,0 +1,12 @@
+class aa{
+    constructor(text){{this.text=text}
+}
+get html(){
+    return 11111
+}
+set html(value){
+    this.text=value
+}
+}
+var bb=new aa('ni hao')
+console.log(bb.text=2222)
